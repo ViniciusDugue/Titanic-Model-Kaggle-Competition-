13525dddb0be18ebcba3438d4f9152c272432828
Vinicius <vinicusdugue08@gmail.com>
2023-02-01 00:33:37 -0800

    Tuessday Jan/31/2023 Schoolday Edits

--- titanic_model_4.py
+++ titanic_model_4.py
@@ -12,44 +12,97 @@ from sklearn.decomposition import PCA
 import matplotlib
 import matplotlib.pyplot
 import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
 
-#Load in csv file into pandas
-train_data = pd.read_csv(r'C:\Users\Vinicius Dugue\Desktop\Titanic Competition(Kaggle)\train.csv')
-test_data = pd.read_csv(r'C:\Users\Vinicius Dugue\Desktop\Titanic Competition(Kaggle)\test.csv')
 
-#Clean Data/(X_train, y_train, X_valid, y_valid, my_cols)
-Names = train_data['Name'].to_numpy()
-X_train_full = train_data.drop(['Survived','Cabin','Name'],axis=1)
-y_train = train_data.Survived
+pd.options.display.max_columns = None
+#PC files
+# train_data = pd.read_csv(r'C:\Users\Vinicius Dugue\Desktop\Titanic Competition(Kaggle)\train.csv')
+# test_data = pd.read_csv(r'C:\Users\Vinicius Dugue\Desktop\Titanic Competition(Kaggle)\test.csv')
+#Laptop files
+train_data = pd.read_csv(r'C:\Users\vinic\OneDrive\Desktop\Titanic Competition(Kaggle)\train.csv')
+test_data = pd.read_csv(r'C:\Users\vinic\OneDrive\Desktop\Titanic Competition(Kaggle)\test.csv')
 
-X_test = test_data.drop(['Name','Cabin'],axis=1)
 
-#Organize data into categorical_cols+numerical_cols
-categorical_cols = [cname for cname in X_train_full.columns if X_train_full[cname].nunique() < 10 and X_train_full[cname].dtype == "object"]
-numerical_cols = [cname for cname in X_train_full.columns if X_train_full[cname].dtype in ['int64', 'float64']]
-my_cols = categorical_cols + numerical_cols
-X_train = X_train_full[my_cols].copy()
+Full_data = pd.concat([train_data, test_data])
 
-numerical_transformer = SimpleImputer(strategy='mean')
-categorical_transformer = Pipeline(steps=[('imputer', SimpleImputer(strategy='most_frequent')), ('onehot', OneHotEncoder(handle_unknown='ignore'))])
+#Impute Missing Age by grouping by Sex, Pclass and using the median of the group
+age_by_pclass_sex = Full_data.groupby(['Sex', 'Pclass']).median()['Age']
+for pclass in range(1, 4):
+    for sex in ['female', 'male']:
+        print(f'Median age of Pclass {pclass} {sex}s: {age_by_pclass_sex[sex][pclass]}')
+print(f"Median age of all passengers: {Full_data['Age'].median()}")
 
-numerical_transformer.fit(X_train[numerical_cols])
-categorical_transformer.fit(X_train[categorical_cols])
-numerical_transformer.transform(X_train[numerical_cols])
-categorical_transformer.transform(X_train[categorical_cols])
+#Impute 2 Missing Embarked
+Full_data['Embarked'] = Full_data['Embarked'].fillna('S')
 
-model = xgb.XGBClassifier(max_depth=5, n_estimators = 500, learning_rate = 0.01, random_state = 0)
+#Impute 1 Missing Fare by grouping by pclass,parch, sibSp and using the median of the group
+med_fare = Full_data.groupby(['Pclass', 'Parch', 'SibSp']).Fare.median()[3][0][0]
+Full_data['Fare']=Full_data['Fare'].fillna(med_fare)
 
-model.fit(X_train, y_train)
 
-print(type(model))
-survivors = pd.dataframe(y_train, columns = ['Survived'])
+#Creating Feature(Deck) by taking first letter of Cabin and if null imputing with 'M'
+Full_data['Deck'] = Full_data['Cabin'].apply(lambda s: s[0] if pd.notnull(s) else 'M')
 
-final_df = pd.concat([X_train, survivors])
+# One passenger in Deck T is 1st class so change to Deck A for ease
+idx = Full_data[Full_data['Deck'] == 'T'].index
+Full_data.loc[idx, 'Deck'] = 'A'
 
-plt.scatter(x=X_train['Pclass'], y = X_train['Age'], label = Names)
-plt.title('Scatter plot of Pclass and Sex')
-plt.xlabel('Pclass')
-plt.ylabel('Sex')
+#Create bar graph displaying survival counts for each deck
+Bar_data = Full_data.dropna(subset=['Survived'])
+Bar_data['Survived'] = Bar_data['Survived'].astype(int)
+
+print(Bar_data['Survived'])
+deck_survived = {'A':[0,0],'B':[0,0],'C':[0,0],'D':[0,0],'E':[0,0],'F':[0,0],'G':[0,0]}
+
+for index, row in Bar_data.iterrows():
+    deck = row['Deck']
+    survived = row['Survived']
+    if deck in deck_survived:
+        deck_survived[deck][survived]+=1
+
+decks = list(deck_survived.keys())
+survived_count = [deck_survived[deck][1] for deck in decks]
+not_survived_count = [deck_survived[deck][0] for deck in decks]
+
+fig,ax = plt.subplot()
+ax.bar(decks, survived_count, label='Survived')
+ax.bar(decks, not_survived_count, bottom=survived_count, label='Not Survived')
+
+ax.set_ytitle('Survival Count')
+ax.set_xtitle('Deck')
+ax.set_title('Survival Counts for Each Deck')
+
+
+
+
+
+# df_all_decks_survived = Full_data.groupby(['Deck', 'Survived']).count().drop(columns=['Sex', 'Age', 'SibSp', 'Parch', 'Fare', 
+# 'Embarked', 'Pclass', 'Cabin', 'PassengerId', 'Ticket']).rename(columns={'Name':'Count'}).transpose()
+# print(['Deck', 'Survived'].count())
+
+# X_train_full = Full_data.iloc[0:len(train_data)].drop(['Survived'], axis=1)
+# X_train = train_data.drop(['Survived'],axis=1)
+# y_train = train_data.Survived
+# X_test = Full_data.iloc[len(train_data):len(Full_data)]
+
+# #Organize data into categorical_cols+numerical_cols
+# categorical_cols = [cname for cname in X_train_full.columns if X_train_full[cname].nunique() < 10 and X_train_full[cname].dtype == "object"]
+# numerical_cols = [cname for cname in X_train_full.columns if X_train_full[cname].dtype in ['int64', 'float64']]
+# my_cols = categorical_cols + numerical_cols
+# X_train = X_train_full[my_cols].copy()
+
+# numerical_transformer = SimpleImputer(strategy='mean')
+# categorical_transformer = Pipeline(steps=[('imputer', SimpleImputer(strategy='most_frequent')), ('onehot', OneHotEncoder(handle_unknown='ignore'))])
+
+# numerical_transformer.fit(X_train[numerical_cols])
+# categorical_transformer.fit(X_train[categorical_cols])
+# numerical_transformer.transform(X_train[numerical_cols])
+# categorical_transformer.transform(X_train[categorical_cols])
+
+# model = xgb.XGBClassifier(max_depth=5, n_estimators = 500, learning_rate = 0.01, random_state = 0)
+
+# model.fit(X_train, y_train)
+
 plt.show()
-#first edit
--- titanic_model_graphs.py
+++ titanic_model_graphs.py
@@ -13,7 +13,7 @@ import matplotlib.pyplot
 import matplotlib.pyplot as plt
 import missingno as msno
 import seaborn as sns
-
+import numpy as np
 #Load in csv file into pandas
 train_data = pd.read_csv(r'C:\Users\vinic\OneDrive\Desktop\Titanic Competition(Kaggle)\train.csv')
 test_data = pd.read_csv(r'C:\Users\vinic\OneDrive\Desktop\Titanic Competition(Kaggle)\test.csv')
@@ -31,6 +31,7 @@ X_train_full = train_data.drop(['Survived','Cabin','Name'],axis=1)
 y_train = train_data.Survived
 
 X_test = test_data.drop(['Name','Cabin'],axis=1)
+Full_data = pd.concat([train_data, test_data]).drop(['Survived', 'Alone'], axis=1)
 
 #Organize data into categorical_cols+numerical_cols
 categorical_cols = [cname for cname in X_train_full.columns if X_train_full[cname].nunique() < 10 and X_train_full[cname].dtype == "object"]
@@ -42,23 +43,29 @@ survivors = pd.DataFrame(y_train, columns = ['Survived'])
 
 final_df = pd.concat([X_train, survivors])
 
+#Correlation Matrix with Heatmap
+corr = Full_data.corr()
+mask = np.triu(np.ones_like(corr, dtype=bool))
+f, ax = plt.subplots(figsize=(11, 9))
+cmap = sns.diverging_palette(230, 20, as_cmap=True)
+sns.heatmap(corr, mask=mask, cmap=cmap, vmax=.3, center=0,
+            square=True, linewidths=.5, cbar_kws={"shrink": .5}, annot=True)
 
+#Calculating Values for Piecharts
 num_children = len(train_data[train_data['Age'] < 18])
 num_men = len(train_data[(train_data['Sex'] == 'male') & (train_data['Age'] > 17)])
 num_women = len(train_data[(train_data['Sex'] == 'female') & (train_data['Age'] > 17)])
 x1 = [num_men, num_children, num_women]
-
 num_children_survived = train_data.query("Survived == 1 & Age < 18").shape[0]
 num_men_survived = train_data.query("Survived == 1 & Sex == 'male' & Age >= 18").shape[0]
 num_women_survived = train_data.query("Survived == 1 & Sex == 'female' & Age >= 18").shape[0]
 x2 = [num_men_survived, num_children_survived, num_women_survived]
-
 num_children_died = train_data.query("Survived == 0 & Age < 18").shape[0]
 num_men_died = train_data.query("Survived == 0 & Sex == 'male' & Age >= 18").shape[0]
 num_women_died = train_data.query("Survived == 0 & Sex == 'female' & Age >= 18").shape[0]
 x3 = [num_men_died, num_children_died, num_women_died]
 
-
+#Creating figure/ for loop to plot all piecharts
 fig, axs = plt.subplots(3, 3, figsize=(7,12))
 fig.subplots_adjust(wspace=1)
 labels = ['Men', 'Children', 'Women']
@@ -68,25 +75,23 @@ for i in range(len(x)):
     axs[i][0].pie(x[i], labels=labels, autopct='%1.1f%%', radius = 1.6, textprops={'fontsize': 7} )
     axs[i][0].set_title(titlez[i],fontsize=7, loc ='center')
 
-# axs[0][0].pie(x1, labels=labels, autopct='%1.1f%%', radius = 1.7, textprops={'fontsize':7})
-# axs[0][0].set_title('# of Total Men/Women/Children',fontsize=7)
-
-# axs[1][0].pie(x2, labels=labels, autopct='%1.1f%%', radius = 1.7, textprops={'fontsize': 14})
-# axs[1][0].set_title('# of Men/Women/Children who Survived',fontsize=7)
-
-# axs[2][0].pie(x3, labels=labels, autopct='%1.1f%%', radius = 1.7, textprops={'fontsize': 14})
-# axs[2][0].set_title('# of Men/Women/Children who died',fontsize=7)
-
-plt.subplots_adjust(wspace=1.5)
-msno.matrix(train_data, figsize=[8, 8], fontsize=10)
-
+#Creating Alone/Not Alone Piechart
 num_alone = train_data.query("Alone == 1").shape[0]
 num_not_alone = train_data.query("Alone == 0").shape[0]
 x4 = [num_alone, num_not_alone]
-
-
 axs[0][1].pie(x4, labels = ['Alone', 'Not Alone'], colors = sns.color_palette('pastel')[0:5], autopct='%1.1f%%', radius = 1.7, textprops={'fontsize': 14})
+
+#Creating bar plot for all missing values of data
+plt.subplots_adjust(wspace=1.5)
+msno.matrix(Full_data, figsize=[8, 8], fontsize=10)
+
 plt.show()
+
+# plt.scatter(x=X_train['Pclass'], y = X_train['Age'], label = Names)
+# plt.title('Scatter plot of Pclass and Sex')
+# plt.xlabel('Pclass')
+# plt.ylabel('Sex')
+# plt.show()
 
 #first edit
 #second edit
